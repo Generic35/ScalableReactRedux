a6693fbde9aac874594a7c834c29b9ea8b09a9c5
Lamontagne <elamonta@allegisgroup.com>
2017-05-09 16:49:34 -0400

    Relaced a component by props.children passed via routing.

--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -9,7 +9,7 @@ import React from 'react';
 import Link from '../Link';
 import styles from './styles.css';
 
-function LinkList({ links }) {
+function LinkList({ links, routeTopicName }) {
   const linkNodes = links.map(l => (
     <Link
       key={l.id}
@@ -20,11 +20,13 @@ function LinkList({ links }) {
   return (
     <div className={styles.linkList}>
       {linkNodes}
+      {routeTopicName}
     </div>
   );
 }
 
 LinkList.propTypes = {
+  routeTopicName: React.PropTypes.string.isRequired,
   links: React.PropTypes.arrayOf(React.PropTypes.shape({
     description: React.PropTypes.string.isRequired,
     url: React.PropTypes.string.isRequired,
