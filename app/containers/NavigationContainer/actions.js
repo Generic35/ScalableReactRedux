bcc4d3bcf1372382d120b73c326dce862bd203c4
Lamontagne <elamonta@allegisgroup.com>
2017-05-09 10:40:22 -0400

    Added a new action creater, and corresponding action constant. Then finally dispatched this action freator from the mapDispatchToProps of the NavigationContainer.

--- app/containers/NavigationContainer/actions.js
+++ app/containers/NavigationContainer/actions.js
@@ -8,6 +8,7 @@ import {
   REQUEST_TOPICS,
   REQUEST_TOPICS_SUCCEEDED,
   REQUEST_TOPICS_FAILED,
+  SELECT_TOPIC,
 } from './constants';
 
 export function requestTopics() {
@@ -26,5 +27,12 @@ export function requestTopicsFailed(message) {
   return {
     type: REQUEST_TOPICS_FAILED,
     message,
+  };
+}
+
+export function selectTopic(topic){
+  return {
+    type: SELECT_TOPIC,
+    topic,
   };
 }
--- app/containers/NavigationContainer/index.js
+++ app/containers/NavigationContainer/index.js
@@ -9,7 +9,7 @@ import { connect } from 'react-redux';
 import selectNavigationContainer from './selectors';
 import styles from './styles.css';
 import Navigation from '../../components/Navigation';
-import { requestTopics } from './actions';
+import { requestTopics, selectTopic } from './actions';
 
 export class NavigationContainer extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
@@ -35,7 +35,7 @@ const mapStateToProps = selectNavigationContainer();
 function mapDispatchToProps(dispatch) {
   return {
     requestTopics: () => dispatch(requestTopics()),
-    selectTopic: (topic) => { console.log('SELECTED TOPIC: ', topic); },
+    selectTopic: (topic) => dispatch(selectTopic(topic)),
   };
 }
 
